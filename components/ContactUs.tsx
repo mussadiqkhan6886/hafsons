bfa67fa0e6b81472844e36cd019e48758b9fe212
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-01 17:21:10 +0500

    contact form

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Title from './Title'
 
 const ContactUs = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <Title content='Get in touch' />
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati voluptatibus cumque, quam consequatur ad iure vel sit quas corrupti incidunt, dignissimos minima dolore? Facilis hic mollitia quam amet numquam debitis?</p>
+      <form>
+        <label htmlFor=""></label>
+      </form>
+    </section>
   )
 }
 
