177064e5f0741b8a7e0712c81f0ac2e866d6d130
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-01 21:15:51 +0500

    valu resp

--- components/Valuation.tsx
+++ components/Valuation.tsx
@@ -4,8 +4,8 @@ import { instrumentSerif } from '@/fonts/font'
 const Valuation = () => {
   return (
     <section className='bg-amber-100 py-10 relative flex items-center  flex-col justify-center'>
-      <h4 className={`font-thin text-4xl mb-3 ${instrumentSerif.className}`}>Request Your free, no-obligation valuation</h4>
-      <p className='text-center font-light mb-4 px-30'>Discover the true value of your assets. Get a complimentary valuation today. Simply fill out the form below, and our experts will provide you with an accurate assessment.</p>
+      <h4 className={`font-thin text-4xl mb-3 text-center ${instrumentSerif.className}`}>Request Your free, no-obligation valuation</h4>
+      <p className='text-center font-light mb-4 px-5 md:px-30'>Discover the true value of your assets. Get a complimentary valuation today. Simply fill out the form below, and our experts will provide you with an accurate assessment.</p>
       <button className='px-6 py-2 text-white bg-yellow-700'>BOOK A VALUATION</button>
     </section>
   )
